fe62a4fd00ae398e1c10b1405fe09e9340de8a1e
kiprono <kiprono_emmanuel@yahoo.com>
2021-11-30 21:22:07 +0300

    add role

---
+++ app/admin/forms.py
@@ -0,0 +1,13 @@
+# app/admin/forms.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class RoleForm(FlaskForm):
+    """
+    Form for admin to add or edit a role
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
---
+++ app/admin/views.py
@@ -0,0 +1,107 @@
+# app/admin/views.py
+
+from flask import abort, flash, redirect, render_template, url_for
+from flask_login import current_user, login_required
+
+from . import admin
+from .forms import RoleForm
+from .. import db
+from ..models import Role
+
+def check_admin():
+    """
+    Prevent non-admins from accessing the page
+    """
+    if not current_user.is_admin:
+        abort(403)
+
+# Role Views
+
+@admin.route('/roles', methods=['GET', 'POST'])
+@login_required
+def list_roles():
+    """
+    List all roles
+    """
+    check_admin()
+
+    roles = Role.query.all()
+
+    return render_template('admin/roles/roles.html',
+                           roles=roles, title="Roles")
+
+@admin.route('/roles/add', methods=['GET', 'POST'])
+@login_required
+def add_role():
+    """
+    Add a role to the database
+    """
+    check_admin()
+
+    add_role = True
+
+    form = RoleForm()
+    if form.validate_on_submit():
+        role = Role(name=form.name.data,
+                                description=form.description.data)
+        try:
+            # add role to the database
+            db.session.add(role)
+            db.session.commit()
+            flash('You have successfully added a new role.')
+        except:
+            # in case role name already exists
+            flash('Error: Role name already exists.')
+
+        # redirect to roles page
+        return redirect(url_for('admin.list_roles'))
+
+    # load role template
+    return render_template('admin/roles/role.html', action="Add",
+                           add_role=add_role, form=form,
+                           title="Add Role")
+
+@admin.route('/roles/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_role(id):
+    """
+    Edit a role
+    """
+    check_admin()
+
+    add_department = False
+
+    role = Role.query.get_or_404(id)
+    form = RoleForm(obj=role)
+    if form.validate_on_submit():
+        role.name = form.name.data
+        role.description = form.description.data
+        db.session.commit()
+        flash('You have successfully edited the department.')
+
+        # redirect to the roles page
+        return redirect(url_for('admin.list_roles'))
+
+    form.description.data = role.description
+    form.name.data = role.name
+    return render_template('admin/roles/role.html', action="Edit",
+                           add_role=add_role, form=form,
+                           role=role, title="Edit Role")
+
+@admin.route('/roles/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_role(id):
+    """
+    Delete a role from the database
+    """
+    check_admin()
+
+    role = Role.query.get_or_404(id)
+    db.session.delete(role)
+    db.session.commit()
+    flash('You have successfully deleted the role.')
+
+    # redirect to the roles page
+    return redirect(url_for('admin.list_roles'))
+
+    return render_template(title="Delete Role")
