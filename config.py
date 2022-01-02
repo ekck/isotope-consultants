3e39643e5d9d7a23e20e20803d385e914e9ceff5
kiprono <kiprono_emmanuel@yahoo.com>
2022-01-02 10:42:51 +0300

    entry

--- config.py
+++ config.py
@@ -22,8 +22,7 @@ class ProductionConfig(Config):
     """
 
     DEBUG = False
-    SECRET_KEY = 'shsa123!@#%^'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://yzldypyauhqwui:f0a1f63ee99be2f22b5eb8cd90e52296a50dfa31cd959d882052ef4a15948a6d@ec2-3-212-172-25.compute-1.amazonaws.com:5432/d14u68modr0s26'
+    
 
 app_config = {
     'development': DevelopmentConfig,
